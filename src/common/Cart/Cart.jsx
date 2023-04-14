1045676e9ce18e54c206928952bd68e061996ebc
Sumedha Lakmal Bandaranayaka <74480291+sumedha-95@users.noreply.github.com>
2023-04-14 15:25:00 +0530

    added Cart and checkout UI

--- src/common/Cart/Cart.jsx
+++ src/common/Cart/Cart.jsx
@@ -1,84 +1,170 @@
-// import React from "react"
-// import "./style.css"
+import React, { useState } from "react";
+import {
+  Box,
+  Typography,
+  TextField,
+  Button,
+  CircularProgress,
+} from "@mui/material";
+import colors from "../../components/assets/colors";
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
 
-// const Cart = ({ CartItem, addToCart, decreaseQty }) => {
-//   // Stpe: 7   calucate total of items
-//   // const totalPrice = CartItem.reduce((price, item) => price + item.qty * item.price, 0)
-// const totalPrice = CartItem ? CartItem.reduce((price, item) => price + item.qty * item.price, 0) : 0;
-//   // prodcut qty total
-//   return (
-//     <>
-//       <section className='cart-items'>
-//         <div className='container d_flex'>
-//           {/* if hamro cart ma kunai pani item xaina bhane no diplay */}
+const Cart = ({list,addToCart,decreaseQty}) => {
 
-//           <div className='cart-details'>
-//             {CartItem.length === 0 && <h1 className='no-items product'>No Items are add in Cart</h1>}
+const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+    const [errors, setErrors] = useState({});
+    const [loading, setLoading] = useState(false);
+    const [conError, setConError] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
 
-//             {/* yasma hami le cart item lai display garaaxa */}
-//             {CartItem.map((item) => {
-//               const productQty = item.price * item.qty
+    const handleClear = () => { };
+      const handleRegisterSubmit = () => {};
+    
+  const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
-//               return (
-//                 <div className='cart-list product d_flex' key={item.id}>
-//                   <div className='img'>
-//                     <img src={item.cover} alt='' />
-//                   </div>
-//                   <div className='cart-details'>
-//                     <h3>{item.name}</h3>
-//                     <h4>
-//                       ${item.price}.00 * {item.qty}
-//                       <span>${productQty}.00</span>
-//                     </h4>
-//                   </div>
-//                   <div className='cart-items-function'>
-//                     <div className='removeCart'>
-//                       <button className='removeCart'>
-//                         <i className='fa-solid fa-xmark'></i>
-//                       </button>
-//                     </div>
-//                     {/* stpe: 5
-//                     product ko qty lai inc ra des garne
-//                     */}
-//                     <div className='cartControl d_flex'>
-//                       <button className='incCart' onClick={() => addToCart(item)}>
-//                         <i className='fa-solid fa-plus'></i>
-//                       </button>
-//                       <button className='desCart' onClick={() => decreaseQty(item)}>
-//                         <i className='fa-solid fa-minus'></i>
-//                       </button>
-//                     </div>
-//                   </div>
+  
+  return (
+    <React.Fragment>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: colors.secondary,
+        }}
+      >
+        <Box
+          sx={{
+            my: 6,
+            borderRadius: 4,
+            backgroundColor: colors.white,
+            boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.25)",
+            p: 5,
+            width: 1200,
+          }}
+        >
 
-//                   <div className='cart-item-price'></div>
-//                 </div>
-//               )
-//             })}
-//           </div>
+          <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={4}>
+         
+             <Typography
+              variant="h6"
+              fontWeight="bold"
+              color="primary"
+              textAlign={"left"}
+              
+              style={{  color: "black" }}
+                  >
+                     <ArrowBackIosNewIcon sx={{ fontSize: 12}} />
+                    Back to shopping
+                   
+            </Typography>
+         
+        </Grid>
+        <Grid item xs={8}>
+            <Box >
+            <Typography
+              variant="h5"
+              fontWeight="bold"
+              color="primary"
+              textAlign={"center"}
+              style={{  color: "black" }}
+            >
+              Cart
+            </Typography>
+          </Box>
+        </Grid>
+              
+        <Grid item xs={12}>
 
-//           <div className='cart-total product'>
-//             <h2>Cart Summary</h2>
-//             <div className=' d_flex'>
-//               <h4>Total Price :</h4>
-//               <h3>${totalPrice}.00</h3>
-//             </div >
-//             <div className='cart-total product'>
+                <section className='cart-items'>
+                  <div className='container d_flex'>
+                    <div className='cart-details'>
+                      {/* {list.length === 0 && <h1 className='no-items product'>No Items are add in Cart</h1>} */}
+                      {list && list.length === 0 && <h1 className='no-items product'>No Items are add in Cart</h1>}
 
-//             <div className=' d_flex'>
-//                 <button className='proceed-checkout'>
-//                   <h2>Proceed to Checkout</h2></button>
-                
-//                 <button type="button" class="btn btn-dark">Proceed to Checkout</button>
-//               </div>
-//               </div>
-//           </div>
+                      
+                       {list && list.map((item) => {
+              const productQty = item.price * item.qty
+
+              return (
+                <div className='cart-list product d_flex' key={item.id}>
+                  <div className='img'>
+                    <img src={item.cover} alt='' />
+                  </div>
+                  <div className='cart-details'>
+                    <h3>{item.name}</h3>
+                    <h4>
+                      ${item.price}.00 * {item.qty}
+                      <span>${productQty}.00</span>
+                    </h4>
+                  </div>
+                  <div className='cart-items-function'>
+                    <div className='removeCart'>
+                      <button className='removeCart'>
+                        <i className='fa-solid fa-xmark'></i>
+                      </button>
+                    </div>
+                    {/* stpe: 5 
+                    product ko qty lai inc ra des garne
+                    */}
+                    <div className='cartControl d_flex'>
+                      <button className='incCart' onClick={() => addToCart(item)}>
+                        <i className='fa-solid fa-plus'></i>
+                      </button>
+                      <button className='desCart' onClick={() => decreaseQty(item)}>
+                        <i className='fa-solid fa-minus'></i>
+                      </button>
+                    </div>
+                  </div>
+
+                  <div className='cart-item-price'></div>
+                </div>
+              )
+                       })}
+                      
+                    </div>
+
+                    <div className='cart-total product'>
+            <h2>Cart Summary</h2>
+            <div className=' d_flex'>
+              <h4>Total Price :</h4>
+              {/* <h3>${totalPrice}.00</h3> */}
+            </div>
+          </div>
+                </div>
+                </section>       
+               
+        </Grid>
+        {/* <Grid item xs={8}>
+          <Item>xs=4</Item>
+        </Grid> */}
+        
+
+      </Grid>
+            
+    </Box>
+
           
-//         </div>
-//       </section>
-//     </>
-//   )
-// }
+        </Box>
+      </Box>
+    </React.Fragment>
+  );
+};
 
-// export default Cart
-
+export default Cart;
 
